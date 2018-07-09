604d337668802c59f318773cb3c13b93fb60c4d9
vmsgiridhar <vmsgbtechgiet@gmail.com>
2018-07-09 19:28:07 +0530

    Uploading Heroku and project files

---
+++ server.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+import json
+import requests
+import os
+app = Flask(__name__)
+#port = '5000'
+port = int(os.environ["PATH"])
+
+@app.route('/', methods=['POST'])
+def index():
+  data = json.loads(request.get_data())
+
+  # FETCH THE CRYPTO NAME
+  crypto_name = data['conversation']['memory']['crypto']['raw']
+
+  # FETCH BTC/USD/EUR PRICES
+  r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_name+"&tsyms=BTC,USD,EUR")
+
+  return jsonify(
+    status=200,
+    replies=[{
+      'type': 'text',
+      'content': 'The price of %s is %f BTC and %f USD' % (crypto_name, r.json()['BTC'], r.json()['USD'])
+    }]
+  )
+
+@app.route('/errors', methods=['POST'])
+def errors():
+  print(json.loads(request.get_data()))
+  return jsonify(status=200)
+
+#app.run(port=port)
+app.run(port=port, host="0.0.0.0")
+#https://giridev1c5232886trial.hanatrial.ondemand.com/ChatBot_DEV/Read.xsjs?EMPID=1002193
+#crypto_name = data['conversation']['memory']['empid']['raw']
