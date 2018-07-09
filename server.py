903e752fdd20a198f3b80322cb40c40f44f3f905
vmsgiridhar <vmsgbtechgiet@gmail.com>
2018-07-09 21:41:14 +0530

    Error 503 :(

--- server.py
+++ server.py
@@ -17,7 +17,8 @@ def index():
 
   # FETCH BTC/USD/EUR PRICES
   #r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_name+"&tsyms=BTC,USD,EUR")
-  r = requests.get("https://giridev1c5232886trial.hanatrial.ondemand.com/ChatBot_DEV/Read.xsjs?EMPID="+crypto_name)
+  #r = requests.get("https://giridev1c5232886trial.hanatrial.ondemand.com/ChatBot_DEV/Read.xsjs?EMPID="+crypto_name)
+  r = requests.get("https://api.myjson.com/bins/l5rbi")
 
   return jsonify(
     status=200,
@@ -37,3 +38,4 @@ app.run(port=port, host="0.0.0.0")
 #https://giridev1c5232886trial.hanatrial.ondemand.com/ChatBot_DEV/Read.xsjs?EMPID=1002193
 #crypto_name = data['conversation']['memory']['empid']['raw']
 #'content': 'The price of %s is %f BTC and %f USD' % (crypto_name, r.json()['BTC'], r.json()['USD'])
+#myjson.com
