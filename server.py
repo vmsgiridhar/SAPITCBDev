51a3aa2632bbfb5e0a12e6f8b7b58bc02c9bdc54
Giridhar Varanasi <vmsgbtechgiet@gmail.com>
2018-07-18 11:20:21 +0200

    Code with a payload submission GET

--- server.py
+++ server.py
@@ -22,7 +22,7 @@ def index():
   #r = requests.get("https://min-api.cryptocompare.com/data/price?fsym="+crypto_name+"&tsyms=BTC,USD,EUR")
   r = requests.get("https://giridev1c5232886trial.hanatrial.ondemand.com/ChatBot_DEV/Read.xsjs?EMPID="+crypto_name_test)
   print(r.url)
-  r1 = requests.get("https://giridev1c5232886trial.hanatrial.ondemand.com/ChatBot_DEV/Read.xsjs",params = postdata)
+  r1 = requests.get("https://giridev1c5232886trial.hanatrial.ondemand.com/ChatBot_DEV/ReadGeneric.xsjs",params = postdata)
   print(r1.url)
   #r = requests.get("https://api.myjson.com/bins/13jh5m")
 
