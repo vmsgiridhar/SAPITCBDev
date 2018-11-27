b7218caf3b6ea3cc01d6a5b5845d4a0020340dd7
Giridhar Varanasi <vmsgbtechgiet@gmail.com>
2018-11-27 21:38:08 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,37 +1,6 @@
-#Working code
-from flask import Flask, request, jsonify 
-from flask_socketio import send, emit    
-import json
-import requests
-import os
-app = Flask(__name__)
-#port = '5000'
-port = int(os.environ["PORT"])
+from flask_socketio import send, emit
 
-@app.route('/', methods=['POST'])
-def index():
-  data = json.loads(request.get_data())
-  
-  #Code for testing the Slack API
-  #challenge_var = data['challenge']
-  test_message_channel = data['event']['text']
-  print(test_message_channel)
-  return jsonify(
-        status=200,
-        replies=[{
-         'text': test_message_channel
-        }]
-      )
-
-json = [{'Test':'Hi Test'}]
-@socketio.on('json')
-def handle_json(json):
-  send(json, json=True)
-
-@app.route('/errors', methods=['POST'])
-def errors():
-  print(json.loads(request.get_data()))
-  return jsonify(status=200)
-
-#app.run(port=port)
-app.run(port=port, host="0.0.0.0") 
+@socketio.on('message')
+def handle_message(message):
+    send(message)
+handle_message("Hi")
