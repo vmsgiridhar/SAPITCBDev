f111edf58a763f5ecb9b81c7286f0c0b990d14fd
Giridhar Varanasi <vmsgbtechgiet@gmail.com>
2018-12-04 15:59:23 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,28 +1,44 @@
-from flask import Flask, request, jsonify 
-from flask_socketio import send, emit, SocketIO   
-import json
-import requests
-import os
-app = Flask(__name__)
-#port = '5000'
-port = int(os.environ["PORT"])
+# from flask import Flask, request, jsonify 
+# from flask_socketio import send, emit, SocketIO   
+# import json
+# import requests
+# import os
+# app = Flask(__name__)
+# #port = '5000'
+# port = int(os.environ["PORT"])
 
-@app.route('/', methods=['POST'])
-def index():
-  data = json.loads(request.get_data())
-  test_message_channel = data['event']['text']
-  print(test_message_channel)
-  return jsonify(
-        status=200,
-        replies=[{
-         'text': test_message_channel
-        }]
-      )
+# @app.route('/', methods=['POST'])
+# def index():
+#   data = json.loads(request.get_data())
+#   test_message_channel = data['event']['text']
+#   print(test_message_channel)
+#   return jsonify(
+#         status=200,
+#         replies=[{
+#          'text': test_message_channel
+#         }]
+#       )
     
-@app.route('/errors', methods=['POST'])
-def errors():
-  print(json.loads(request.get_data()))
-  return jsonify(status=200)
+# @app.route('/errors', methods=['POST'])
+# def errors():
+#   print(json.loads(request.get_data()))
+#   return jsonify(status=200)
 
-#app.run(port=port)
-app.run(port=port, host="0.0.0.0") 
+# #app.run(port=port)
+# app.run(port=port, host="0.0.0.0") 
+
+# Basic Flask Python Web App
+from flask import Flask
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5a1b8a0f3c'
+socketio = SocketIO(app)
+
+#@socketio.on('message')
+#def messageHandler(msg):
+#    print('Message: '+ msg)
+#    send(msg, broadcast=True)
+
+if __name__ == "__main__":
+    socketio.run(app)
